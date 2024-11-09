b03f01b7bb2fc339090a8a14347b3265a769e13a
turbogorogoso <naddanai_turbo@outlook.com>
2024-11-10 03:37:30 +0700

    cron

---
+++ dags/cron.py
@@ -0,0 +1,23 @@
+from airflow import DAG
+from datetime import datetime, timedelta
+from airflow.operators.bash import BashOperator
+
+default_args = {
+    'owner' : 'ndst',
+    'retries' : 5,
+    'retry_delay' : timedelta(minutes = 2)
+}
+
+with DAG(
+    dag_id = "cron3",
+    default_args = default_args,
+    description = "heekuytat",
+    start_date = datetime(2024, 10, 20),
+    schedule_interval='0 3 * * Tue-Fri'
+
+) as dag:
+    task1 = BashOperator(
+        task_id = 'task1',
+        bash_command="echo croncroncroncroncroncroncron"
+    )
+    task1
